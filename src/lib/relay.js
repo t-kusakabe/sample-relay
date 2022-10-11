523cc6902867a7d9266a50813dd7df6343126243
t-kusakabe <ttmac0704@gmail.com>
2022-10-11 22:11:03 +0900

    relay周りの設定

---
+++ src/lib/relay.js
@@ -0,0 +1,45 @@
+import { useMemo } from 'react';
+import { Environment, Network, RecordSource, Store } from 'relay-runtime';
+
+let relayEnvironment;
+
+const fetchQuery = (operation, variables, cacheConfig, uploadables) => {
+  const Token = process.env.NEXT_PUBLIC_GITHUB_AUTH_TOKEN;
+
+  return fetch(process.env.NEXT_PUBLIC_RELAY_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `bearer ${Token}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables
+    })
+  }).then((response) => response.json());
+};
+
+const createEnvironment = (initialRecords) => {
+  return new Environment({
+    network: Network.create(fetchQuery),
+    store: new Store(new RecordSource())
+  });
+};
+
+export const initEnvironment = (initialRecords) => {
+  const environment = relayEnvironment ?? createEnvironment(initialRecords);
+
+  if (initialRecords) {
+    environment.getStore().publish(new RecordSource(initialRecords));
+  }
+  if (typeof window === 'undefined') return environment;
+  if (!relayEnvironment) relayEnvironment = environment;
+
+  return relayEnvironment;
+};
+
+export const useEnvironment = (initialRecords) => {
+  const store = useMemo(() => initEnvironment(initialRecords), [initialRecords]);
+  return store;
+};
